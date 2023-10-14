57593aff4909f803135daaa6741ab9e244a43821
Norman Torres <46954023+dataNorman@users.noreply.github.com>
2023-10-14 11:55:01 -0500

    deploy de k8s

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello new World 2');
+  res.send('Hello new World 3');
 })
 
 module.exports = app;
